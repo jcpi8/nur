0e1396bdef44667827905e72039d546392c8d610
Jordan C. Pierce <jdan.pier@gmail.com>
2022-12-14 18:56:32 -0800

    Added more to meta attrs

--- pkgs/chatterino7/default.nix
+++ pkgs/chatterino7/default.nix
@@ -28,11 +28,16 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ pkg-config wrapQtAppsHook ];
   buildInputs = [ cmake qttools qtbase qtsvg qtmultimedia openssl boost ];
   meta = with lib; {
-    description = "Twitch client, Chatterino2 fork that supports 7TV emotes.";
+    description = "Dedicated twitch chat client, with experimental support for 7TV emotes.";
+    longDescription = ''
+      Chatterino7 is a Chatterino2 fork that supports 7TV emotes.  Since it
+      largely uses the codebase of Chatterino2, it reuses the settings set by
+      Chatterino2. This makes it easy to switch-in and switch-out.
+    '';
     homepage = "https://www.github.com/SevenTV/chatterino7";
     license = licenses.mit;
-    platforms = platforms.all;
-    # QtWebkit upstream is unmaintained and receives no security updates, see
-    # https://blogs.gnome.org/mcatanzaro/2022/11/04/stop-using-qtwebkit/
+    # After viewing the chatterino2 package, it seems like I'm missing some of
+    # the niceties offered to MacOS.
+    platforms = platforms.linux;
   };
 }
